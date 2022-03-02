2066f4f3dc320e911d92e55ea0953e88fa3611b8
verycosy <verycosy@kakao.com>
2022-03-02 17:08:29 +0900

    feat: 사이드메뉴 샘플

--- src/components/shared/SideMenu.tsx
+++ src/components/shared/SideMenu.tsx
@@ -1,9 +1,29 @@
-import { Link } from 'react-router-dom';
+import { Menu } from 'antd';
+import { Link, useLocation } from 'react-router-dom';
+import {
+  UserOutlined,
+  ShopOutlined,
+  HomeOutlined,
+  NotificationOutlined,
+} from '@ant-design/icons';
 
 export const SideMenu: React.FC = () => {
+  const { pathname } = useLocation();
+
   return (
-    <nav>
-      <Link to="/users">회원관리</Link>
-    </nav>
+    <Menu theme="dark" selectedKeys={[pathname.split('/')[1]]}>
+      <Menu.Item icon={<HomeOutlined />} key="">
+        <Link to="/">홈</Link>
+      </Menu.Item>
+      <Menu.Item icon={<NotificationOutlined />} key="notice">
+        <Link to="/notices">공지사항</Link>
+      </Menu.Item>
+      <Menu.Item icon={<UserOutlined />} key="users">
+        <Link to="/users">회원관리</Link>
+      </Menu.Item>
+      <Menu.Item icon={<ShopOutlined />} key="stores">
+        <Link to="/stores">가게관리</Link>
+      </Menu.Item>
+    </Menu>
   );
 };
